8a267eb37a7e4c8c3ba4831d98d72375276bc148
Dovakin23 <itzsyuk@mail.ru>
2026-06-09 14:00:00 +1000

    Use vector radiation icons throughout UI

--- src/StalkerModLauncher/Infrastructure/BoolToRadiationConverter.cs
+++ src/StalkerModLauncher/Infrastructure/BoolToRadiationConverter.cs
@@ -1,33 +1,21 @@
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
-using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace StalkerModLauncher.Infrastructure;
 
 public sealed class BoolToRadiationConverter : IValueConverter
 {
-    private static readonly BitmapImage OnImage = LoadImage("Resources/radiation_on.png");
-    private static readonly BitmapImage OffImage = LoadImage("Resources/radiation_off.png");
-
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is true ? OnImage : OffImage;
+        var resourceKey = value is true ? "RadiationOnImage" : "RadiationOffImage";
+        return Application.Current?.TryFindResource(resourceKey) as ImageSource
+            ?? DependencyProperty.UnsetValue;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
-    }
-
-    private static BitmapImage LoadImage(string path)
-    {
-        var uri = new Uri($"pack://application:,,,/{path}", UriKind.RelativeOrAbsolute);
-        var image = new BitmapImage();
-        image.BeginInit();
-        image.UriSource = uri;
-        image.CacheOption = BitmapCacheOption.OnLoad;
-        image.EndInit();
-        image.Freeze();
-        return image;
     }
 }
